5c1c2c6769161d7e6062129fa79ba7fc1ff400e4
Fernando MANTILLA <45453385+dmantilla9@users.noreply.github.com>
2024-09-06 12:30:16 +0200

    Create travelers.js

---
+++ travelers.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 19890307,
+    "firstName":  "Fernando",
+    "lastName":   "MANTILLA ARGUELLO",
+    "BirthDate":  new Date('1989-03-07T00:00:00Z'),
+    "FamilyRole": "Dad"
+  },
+  {
+    "id": 19900801,
+    "firstName": "Catalina",
+    "lastName": "MENDEZ CAMPO",
+    "BirthDate": new Date('1990-08-01T00:00:00Z'),
+    "FamilyRole":"Mom"
+  },
+  {
+    "id": 20230211,
+    "firstName": "Nicolas",
+    "lastName": "MANTILLA MENDEZ",
+    "BirthDate":new Date('2023-02-11T09:57:00Z'),
+    "FamilyRole":"Child"
+  }
+]
